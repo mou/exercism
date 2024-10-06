ab99be72b6020458d14c666837a51d98fdaa7ffe
Andrey "MOU" Larionov <anlarionov@gmail.com>
2024-10-06 14:00:12 +0200

    Updated versions of compilers and interpreters

---
+++ r/flake.nix
@@ -0,0 +1,22 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {self, nixpkgs, utils}:
+    let out = system:
+    let pkgs = nixpkgs.legacyPackages."${system}";
+    in {
+
+        devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              R
+	      rPackages.testthat
+	      exercism
+            ];
+        };
+
+    }; in with utils.lib; eachSystem defaultSystems out;
+
+}
