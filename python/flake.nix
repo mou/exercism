3a5b19025e40c38051b715e51655ac9962876728
Andrey "MOU" Larionov <anlarionov@gmail.com>
2024-12-24 21:53:17 +0100

    Python environment and hello-world

---
+++ python/flake.nix
@@ -0,0 +1,23 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+        utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {self, nixpkgs, utils}:
+    let out = system:
+    let pkgs = nixpkgs.legacyPackages."${system}";
+    in {
+
+        devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              python312
+              python312Packages.pytest
+              python312Packages.pytest-cache
+              python312Packages.pytest-subtests
+            ];
+        };
+
+    }; in with utils.lib; eachSystem defaultSystems out;
+
+}
