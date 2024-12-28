94fbb5610dee77fd8e67aaa62220292b25bd9481
Andrey "MOU" Larionov <anlarionov@gmail.com>
2024-12-28 22:56:00 +0100

    CFML environment and hello world

---
+++ cfml/flake.nix
@@ -0,0 +1,20 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+        utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {self, nixpkgs, utils}:
+    let out = system:
+    let pkgs = nixpkgs.legacyPackages."${system}";
+    in {
+
+        devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              jdk11
+            ];
+        };
+
+    }; in with utils.lib; eachSystem defaultSystems out;
+
+}
