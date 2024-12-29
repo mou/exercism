cb448e832f7348ad2a39bdee7f0a2a87d7d3afb4
Andrey "MOU" Larionov <anlarionov@gmail.com>
2024-12-29 18:56:35 +0100

    COBOL environemt and hello world

---
+++ cobol/flake.nix
@@ -0,0 +1,23 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+        utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {self, nixpkgs, utils}:
+    let out = system:
+    let pkgs = nixpkgs.legacyPackages."${system}";
+    in {
+
+        devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              gnucobol
+            ];
+            shellHook = ''
+              export PATH="${pkgs.gnucobol}/bin:$PATH";
+            '';
+        };
+
+    }; in with utils.lib; eachSystem defaultSystems out;
+
+}
